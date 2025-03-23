89ff49d24cb732c45e60f6ffe104591bce69843d
TurtleStoffel <me@turtlestoffel.com>
2025-03-23 19:53:28 +0100

    Read experiments from YAML file

--- README.md
+++ README.md
@@ -6,3 +6,43 @@ To execute the script, run
 ```sh
 deno task start
 ```
+
+## Experiments
+Experiments are defined in the `experiments` folder. Each experiment is a YAML file with the following fields:
+- `models`: List of models to compare
+- `stylePrompt`: Prompt to specify the style used by the LLM to generate text
+- `llmPrompt`: Prompt to specify the text to generate
+
+Each experiment should have a unique name that represents what the experiment is about.
+
+An example of an experiment file named `rewrite-bullet-points-to-text.yaml`:
+```yaml
+models:
+- deepseek-r1
+- gemma3:12b
+- llama3.2
+stylePrompt: |
+  Use clear, direct language and avoid complex terminology.
+  Aim for a Flesch reading score of 80 or higher.
+  Use the active voice.
+  Avoid adverbs.
+  Avoid buzzwords and instead use plain English.
+  Use jargon where relevant.
+  Avoid being salesy or overly enthusiastic and instead express calm confidence.
+  Prefer full sentences over bullet points unless explicitly requested.
+llmPrompt: |
+  The context is a software system that uses available data to determine the code quality of a codebase.
+  Rewrite the following section into paragraphs without bullet points that could be used in an article about the topic.
+
+  ## Concept
+  Rely as little as possible on gathering manual data from engineering, but rather on other metrics that can easily be calculated from existing systems:
+  - Runtime data
+    - How often are lines executed
+    - How often are certain branches taken
+    - This can be based on automated tests too, although it's not 100% representative
+  - Version Control data
+    - Code hotspots (= how often are certain files updated)
+      - Code hotspots + code coverage by automated tests
+    - How much do tests have to change as a result of a code change (a lot of changing tests is usually a bad sign)
+    - Unchanged code (good code doesn't have to change because it works, bad code usually isn't changed because nobody dares to touch it)
+```
--- main.ts
+++ main.ts
@@ -1,39 +1,21 @@
-const models = ["deepseek-r1"]
+import { parse } from "jsr:@std/yaml"
 
-const stylePrompt = `
-Use clear, direct language and avoid complex terminology.
-Aim for a Flesch reading score of 80 or higher.
-Use the active voice.
-Avoid adverbs.
-Avoid buzzwords and instead use plain English.
-Use jargon where relevant.
-Avoid being salesy or overly enthusiastic and instead express calm confidence.
+const experiment = await Deno.readTextFile("./experiments/rewrite-bullet-points-to-text.yaml")
 
-Avoid using bullet points, instead write out information in full sentences.
-`
+interface Experiment {
+  models: string[]
+  stylePrompt: string
+  llmPrompt: string
+}
 
-const llmPrompt = `
-Rewrite the following section with bullet points into normal text.
+const parsedExperiment = parse(experiment) as Experiment
 
-## Concept
-Rely as little as possible on gathering manual data from engineering, but rather on other metrics that can easily be calculated from existing systems:
-- Runtime data
-	- How often are lines executed
-	- How often are certain branches taken
-	- This can be based on automated tests too, although it's not 100% representative
-- Version Control data
-	- Code hotspots (= how often are certain files updated)
-		- Code hotspots + code coverage by automated tests
-	- How much do tests have to change as a result of a code change (a lot of changing tests is usually a bad sign)
-	- Unchanged code (good code doesn't have to change because it works, bad code usually isn't changed because nobody dares to touch it)
-`
-
-models.forEach(async model => {
+parsedExperiment.models.forEach(async model => {
   let resp = await fetch("http://localhost:11434/api/generate", {
     method: "POST",
     body: JSON.stringify({ 
       model,
-      prompt: stylePrompt + llmPrompt,
+      prompt: parsedExperiment.stylePrompt + parsedExperiment.llmPrompt,
       stream: false,
     }),
   });
@@ -45,5 +27,5 @@ models.forEach(async model => {
 
   const parsedContent = JSON.parse(content)
 
-  await Deno.writeTextFile(`./output/test.txt`, parsedContent.response, { encoding: "utf8" });
+  await Deno.writeTextFile(`./output/test-${model}.txt`, parsedContent.response, { encoding: "utf8" });
 })
